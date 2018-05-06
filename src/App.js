0cc742aad903b895dd23ecc16fcda4f82e2fb6a4
andy <andy.speirs2@gmail.com>
2018-05-06 21:35:42 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { orange500, orange700 } from "material-ui/styles/colors";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import NavBar from "./components/NavBar/NavBar";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    const muiTheme = getMuiTheme({
+      palette: {
+        primary1Color: orange500,
+        primary2Color: orange700
+      }
+    });
+
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div>
+          <NavBar />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import AppBar from "material-ui/AppBar";
+import Drawer from "material-ui/Drawer";
+import MenuItem from "material-ui/MenuItem";
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+
+  handleToggle = () => this.setState({ open: !this.state.open });
+  handleClose = () => this.setState({ open: false });
+
+  render() {
+    return (
+      <div>
+        <AppBar title="List Me" onLeftIconButtonClick={this.handleToggle} />
+        <Drawer
+          docked={false}
+          width={250}
+          open={this.state.open}
+          onRequestChange={open => this.setState({ open })}
+        >
+          <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
+          <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
+        </Drawer>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
