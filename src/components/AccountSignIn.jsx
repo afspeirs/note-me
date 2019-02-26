451dec3c8bb74fbd0fa168d2b0e6f6c4978ba5f5
Andy <andy.speirs2@gmail.com>
2019-02-26 19:57:17 +0000

    Versioned to 1.0.2 Refactored Sign in functionality into a AccountSignIn component

---
+++ src/components/AccountSignIn.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+	List,
+	ListButton,
+	ListGroup,
+	ListItem,
+} from 'framework7-react';
+
+import { auth, provider } from '../firebase';
+import blankUserPhoto from '../img/blank-user-photo.png';
+
+export default class AccountSignIn extends React.Component {
+	state = {
+		user: null,
+	};
+
+	componentDidMount() {
+		auth.onAuthStateChanged((user) => {
+			if (user) {
+				this.setState({ user });
+			}
+		});
+	}
+
+	signOut = () => {
+		auth.signOut()
+			.then(() => this.setState({ user: null }));
+
+		this.$f7router.clearPreviousPages();
+	}
+
+	signIn = () => {
+		auth.signInWithPopup(provider)
+			.then((result) => {
+				const { user } = result;
+				this.setState({ user });
+			});
+	}
+
+	render() {
+		const { user } = this.state;
+		// console.sign(user);
+
+		return (
+			<List>
+				<ListGroup>
+					<ListItem title={user ? user.email : 'Sign in below with Google'}>
+						{user
+							? <img slot="media" className="user-photo" src={user.photoURL} alt="user" />
+							: <img slot="media" className="user-photo" src={blankUserPhoto} alt="user" />
+						}
+					</ListItem>
+				</ListGroup>
+				<ListGroup>
+					{user
+						? <ListButton onClick={this.signOut} title="Sign Out" color="red" />
+						: <ListButton onClick={this.signIn} title="Sign In" color="orange" />
+					}
+				</ListGroup>
+			</List>
+		);
+	}
+}
--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -2,81 +2,29 @@ import React from 'react';
 import {
 	Page,
 	List,
-	ListButton,
 	ListGroup,
 	ListItem,
 } from 'framework7-react';
 
-import { auth, provider } from '../firebase';
-import Navbar from '../components/Navbar';
+import AccountSignIn from '../components/AccountSignIn';
 import CheckForUpdate from '../components/CheckForUpdate';
+import Navbar from '../components/Navbar';
 
-import blankUserPhoto from '../img/blank-user-photo.png';
+export default () => (
+	<Page>
+		<Navbar title="Settings" backLink="Back" />
 
-export default class HomePage extends React.Component {
-	state = {
-		user: null,
-	};
+		<AccountSignIn />
 
-	componentDidMount() {
-		auth.onAuthStateChanged((user) => {
-			if (user) {
-				this.setState({ user });
-			}
-		});
-	}
-
-	logout = () => {
-		auth.signOut()
-			.then(() => this.setState({ user: null }));
-
-		this.$f7router.clearPreviousPages();
-	}
-
-	login = () => {
-		auth.signInWithPopup(provider)
-			.then((result) => {
-				const { user } = result;
-				this.setState({ user });
-			});
-	}
-
-	render() {
-		const { user } = this.state;
-		// console.log(user);
-
-		return (
-			<Page>
-				<Navbar title="Settings" backLink="Back" />
-
-				<List>
-					<ListGroup>
-						<ListItem title={user ? user.email : 'Please log in'}>
-							{user
-								? <img slot="media" className="user-photo" src={user.photoURL} alt="user" />
-								: <img slot="media" className="user-photo" src={blankUserPhoto} alt="user" />
-							}
-						</ListItem>
-					</ListGroup>
-					<ListGroup>
-						{user
-							? <ListButton onClick={this.logout} title="Log Out" color="red" />
-							: <ListButton onClick={this.login} title="Log In" />
-						}
-					</ListGroup>
-				</List>
-
-				<List>
-					<ListGroup>
-						<ListItem title="App Version">
-							{`v${process.env.REACT_APP_VERSION}`}
-						</ListItem>
-					</ListGroup>
-					<ListGroup>
-						<CheckForUpdate />
-					</ListGroup>
-				</List>
-			</Page>
-		);
-	}
-}
+		<List>
+			<ListGroup>
+				<ListItem title="App Version">
+					{`v${process.env.REACT_APP_VERSION}`}
+				</ListItem>
+			</ListGroup>
+			<ListGroup>
+				<CheckForUpdate />
+			</ListGroup>
+		</List>
+	</Page>
+);
