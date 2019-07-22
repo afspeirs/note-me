47634d01aef001ec28e380b9d56880c8118fa749
Andy <andy.speirs2@gmail.com>
2019-07-22 21:50:14 +0100

    Added offline support through enablePersistence

--- src/firebase.js
+++ src/firebase.js
@@ -25,3 +25,22 @@ firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const db = firebase.firestore();
 export const provider = new firebase.auth.GoogleAuthProvider();
+
+// This enables offline support
+// Firebase is smart enough to update the changes when the user
+// goes online again
+firebase.firestore()
+	.enablePersistence()
+	.catch((error) => {
+		if (error.code === 'failed-precondition') {
+			// Multiple tabs open, persistence can only be enabled
+			// in one tab at a a time.
+			// ...
+			console.log('Please close other tabs to allow the app to work offline');
+		} else if (error.code === 'unimplemented') {
+			// The current browser does not support all of the
+			// features required to enable persistence
+			// ...
+			console.log('Offline support is not supported in this browser');
+		}
+	});
