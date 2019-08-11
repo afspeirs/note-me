ebe1b3aebf76c783389b5a31f04a995254dc4a0d
Andy <andy.speirs2@gmail.com>
2019-08-11 19:41:25 +0100

    Updated theme colour to #ee6e00

--- src/theme.js
+++ src/theme.js
@@ -3,11 +3,10 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export default createMuiTheme({
 	palette: {
 		primary: {
-			main: '#ff9500',
+			main: '#ee6e00',
 		},
 	},
 	zIndex: {
 		appBar: 1350,
-		// modal: 1375,
 	},
 });
