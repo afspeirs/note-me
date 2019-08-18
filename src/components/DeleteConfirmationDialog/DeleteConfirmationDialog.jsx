0ac0a2ec3935e0a1b3c5d855cf511c8681411176
Andy <andy.speirs2@gmail.com>
2019-08-18 14:45:38 +0100

    Added DeleteConfirmationDialog component

---
+++ src/components/DeleteConfirmationDialog/DeleteConfirmationDialog.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+	Button,
+	Dialog,
+	DialogActions,
+	DialogTitle,
+} from '@material-ui/core';
+
+const defaultProps = {
+	value: null,
+};
+
+const propTypes = {
+	onClose: PropTypes.func.isRequired,
+	open: PropTypes.bool.isRequired,
+	value: PropTypes.instanceOf(Object),
+};
+
+const DeleteConfirmationDialog = ({
+	onClose,
+	value,
+	open,
+}) => {
+	const handleConfirm = (note = null) => onClose(note);
+
+	return value && (
+		<Dialog
+			maxWidth="xs"
+			aria-labelledby="confirmation-dialog-title"
+			open={open}
+			onClose={onClose}
+		>
+			<DialogTitle id="confirmation-dialog-title">{`Are you sure you want to delete "${value && value.text}"?`}</DialogTitle>
+			<DialogActions>
+				<Button onClick={onClose} color="primary" autoFocus>Cancel</Button>
+				<Button onClick={() => handleConfirm(value)} color="secondary">Delete</Button>
+			</DialogActions>
+		</Dialog>
+	);
+};
+
+DeleteConfirmationDialog.defaultProps = defaultProps;
+DeleteConfirmationDialog.propTypes = propTypes;
+
+export default DeleteConfirmationDialog;
--- src/components/NotesList/NotesList.jsx
+++ src/components/NotesList/NotesList.jsx
@@ -5,6 +5,7 @@ import 'rc-swipeout/dist/rc-swipeout.css';
 import { ListItem } from '@material-ui/core';
 import { Delete as DeleteIcon } from '@material-ui/icons';
 
+import DeleteConfirmationDialog from '../DeleteConfirmationDialog';
 import ListItemLink from './ListItemLink';
 import TimeAgo from '../TimeAgo';
 import { ListStyled, ListItemTextStyled } from './NotesList.styled';
@@ -26,53 +27,77 @@ const NotesList = ({
 	handleNoteDelete,
 	loading,
 	notes,
-}) => (
-	<ListStyled>
-		{notes.length === 0 && loading === false && (
-			<ListItem>
-				<ListItemTextStyled primary="No notes" />
-			</ListItem>
-		)}
-		{loading && (
-			<ListItem>
-				<ListItemTextStyled primary="Loading, please wait while we gather your notes" />
-			</ListItem>
-		)}
-		{notes.map(note => (
-			<Swipeout
-				key={`note-${note.id}`}
-				left={[
-					{
-						text: <TimeAgo date={note.date / 1000} />,
-						autoClose: true,
-						style: {
-							backgroundColor: '#ee6e00',
-							color: 'white',
-						},
-					},
-					{
-						text: <DeleteIcon />,
-						onPress: () => handleNoteDelete(note.id, note),
-						autoClose: true,
-						style: {
-							backgroundColor: 'red',
-							color: 'white',
-							width: '56px',
-						},
-					},
-				]}
-			>
-				<ListItemLink
-					to={`/note/${note.id}`}
-					className="context-menu-select"
-					id={note.id}
-					onClick={handleDrawerToggle}
-					primary={note.text ? getTitle(note.text) : 'Untitled'}
-				/>
-			</Swipeout>
-		))}
-	</ListStyled>
-);
+}) => {
+	const [open, setOpen] = React.useState(false);
+	const [value, setValue] = React.useState(null);
+
+	const handleClose = (note = null) => {
+		if (note && note.id) handleNoteDelete(note.id, note);
+		setOpen(false);
+		setValue(null);
+	};
+
+	const handleOpen = (note) => {
+		setValue(note);
+		setOpen(true);
+	};
+
+	return (
+		<>
+			<ListStyled>
+				{notes.length === 0 && loading === false && (
+					<ListItem>
+						<ListItemTextStyled primary="No notes" />
+					</ListItem>
+				)}
+				{loading && (
+					<ListItem>
+						<ListItemTextStyled primary="Loading, please wait while we gather your notes" />
+					</ListItem>
+				)}
+				{notes.map(note => (
+					<Swipeout
+						key={`note-${note.id}`}
+						left={[
+							{
+								text: <TimeAgo date={note.date / 1000} />,
+								autoClose: true,
+								style: {
+									backgroundColor: '#ee6e00',
+									color: 'white',
+								},
+							},
+							{
+								text: <DeleteIcon />,
+								onPress: () => handleOpen(note),
+								autoClose: true,
+								style: {
+									backgroundColor: 'red',
+									color: 'white',
+									width: '56px',
+								},
+							},
+						]}
+					>
+						<ListItemLink
+							to={`/note/${note.id}`}
+							className="context-menu-select"
+							id={note.id}
+							onClick={handleDrawerToggle}
+							primary={note.text ? getTitle(note.text) : 'Untitled'}
+						/>
+					</Swipeout>
+				))}
+			</ListStyled>
+
+			<DeleteConfirmationDialog
+				open={open}
+				onClose={handleClose}
+				value={value}
+			/>
+		</>
+	);
+};
 
 NotesList.defaultProps = defaultProps;
 NotesList.propTypes = propTypes;
