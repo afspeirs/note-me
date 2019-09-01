db849b6aed46640f6056139436b91068df5bdfd0
Andy Speirs <andy.speirs2@gmail.com>
2019-09-01 14:24:41 +0100

    Update app button (#22)
    * Add CheckForUpdate component
    
    * Created SimpleSnackbar component
    
    * Add event listeners for service worker events
    
    * Added self.skipWaiting to install event
    
    * Added new dependency to project to append code to service worker
    
    * Updated start and build tasks

---
+++ src/components/CheckForUpdate/CheckForUpdate.jsx
@@ -0,0 +1,97 @@
+// import React from 'react';
+// import {
+// 	ListItem,
+// 	ListItemSecondaryAction,
+// 	ListItemText,
+// } from '@material-ui/core';
+
+// export default class CheckForUpdate extends React.Component {
+// 	state = {
+// 		showUpdatePreloader: false,
+// 	};
+
+// 	updateServiceWorker = () => {
+// 		this.setState({ showUpdatePreloader: true });
+
+// 		if ('serviceWorker' in navigator) {
+// 			navigator.serviceWorker.ready.then(registration => registration.update());
+// 		} else {
+// 			setTimeout(() => window.location.reload(window.location.href), 1500);
+// 		}
+
+// 		setTimeout(() => this.setState({ showUpdatePreloader: false }), 1500);
+// 	}
+
+// 	render() {
+// 		const { showUpdatePreloader } = this.state;
+// 		// console.log(showUpdatePreloader);
+
+// 		return (
+// 			<ListItem>
+// 				<ListItemText primary="Check for update" />
+// 				<ListItemSecondaryAction>
+// 					{!showUpdatePreloader ? 'Refresh' : 'loading'}
+// 				</ListItemSecondaryAction>
+// 			</ListItem>
+// 		);
+// 	}
+// }
+
+
+import React from 'react';
+import {
+	CircularProgress,
+	IconButton,
+	ListItem,
+	ListItemSecondaryAction,
+	ListItemText,
+} from '@material-ui/core';
+import {
+	Refresh as RefreshIcon,
+} from '@material-ui/icons';
+
+const CheckForUpdate = () => {
+	const [loading, setLoading] = React.useState(false);
+	const timer = React.useRef();
+
+	// This abominable one-liner will clear the timer if CheckForUpdate unmounts
+	React.useEffect(() => () => clearTimeout(timer.current), []);
+
+	const updateServiceWorker = () => {
+		if ('serviceWorker' in navigator) {
+			navigator.serviceWorker.ready.then(registration => registration.update());
+		} else {
+			setTimeout(() => window.location.reload(window.location.href), 1500);
+		}
+	};
+
+	const handleButtonClick = () => {
+		if (!loading) {
+			setLoading(true);
+			updateServiceWorker();
+			timer.current = setTimeout(() => setLoading(false), 2000);
+		}
+	};
+
+	return (
+		<ListItem>
+			<ListItemText primary="Check for update" />
+			<ListItemSecondaryAction>
+				{loading ? (
+					<CircularProgress size={24} color="inherit" />
+				) : (
+					<IconButton
+						color="inherit"
+						disabled={loading}
+						onClick={handleButtonClick}
+						edge="end"
+					>
+						<RefreshIcon />
+					</IconButton>
+				)}
+			</ListItemSecondaryAction>
+		</ListItem>
+	);
+};
+
+export default CheckForUpdate;
