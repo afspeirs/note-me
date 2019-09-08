3935427ef6a9eaac55d899b246ddaff4027afd57
Andy <andy.speirs2@gmail.com>
2019-09-08 15:25:38 +0100

    Remove notes from state when there is no user

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -35,7 +35,7 @@ export default class App extends Component {
 						this.setState({ loading: false, notes, user });
 					});
 			} else {
-				this.setState({ loading: false });
+				this.setState({ loading: false, notes: [] });
 			}
 		});
 
