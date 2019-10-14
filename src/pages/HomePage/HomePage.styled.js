145e2e886752146e72c396c82f5034d3148fd975
Andy <andy.speirs2@gmail.com>
2019-10-14 21:42:02 +0100

    Updated copy if no note is selected

--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -1,6 +1,13 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
+	centered: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: '100%',
+		height: '100%',
+	},
 	page: {
 		display: 'flex',
 		flexGrow: 1,
