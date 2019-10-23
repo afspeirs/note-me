4b0c2c3c105ed75adc579c4dab4174048278b893
Andy Speirs <andy.speirs2@gmail.com>
2019-10-23 20:24:15 +0100

    Remove delete confirmation dialog component (#53)
    * Changed copy to say Delete note
    
    * Use dialog to generate delete confirmation for swipeout
    
    * Refactored ContextMenu to use material-ui-confirm

--- src/components/NotesList/NotesList.jsx
+++ src/components/NotesList/NotesList.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 import Swipeout from 'rc-swipeout';
 import { List, ListItem, ListItemText } from '@material-ui/core';
 import { Delete as DeleteIcon } from '@material-ui/icons';
+import withConfirm from 'material-ui-confirm';
 
 import useStyles from './NotesList.styled';
 import ContextMenu from '../ContextMenu';
-import DeleteConfirmationDialog from '../DeleteConfirmationDialog';
 import ListItemLink from './ListItemLink';
 import TimeAgo from '../TimeAgo';
 import { useStateValue } from '../StateContext';
 import { getTitle } from '../../ultils';
 
 const propTypes = {
+	confirm: PropTypes.func.isRequired,
 	handleNoteDelete: PropTypes.func.isRequired,
 	loading: PropTypes.bool.isRequired,
 	notes: PropTypes.instanceOf(Array).isRequired,
 };
 
 const NotesList = ({
+	confirm,
 	handleNoteDelete,
 	loading,
 	notes,
 }) => {
 	const classes = useStyles();
-	const history = useHistory()
-	const [open, setOpen] = React.useState(false);
-	const [value, setValue] = React.useState(null);
+	const history = useHistory();
 	const [{ sort }] = useStateValue();
 
 	const sortFunction = {
@@ -36,17 +36,6 @@ const NotesList = ({
 		'title-asc': (a, b) => a.text.localeCompare(b.text),
 		'title-dsc': (a, b) => b.text.localeCompare(a.text),
 	}[sort];
-
-	const handleClose = (note = null) => {
-		if (note && note.id) handleNoteDelete(note.id, history);
-		setOpen(false);
-		setValue(null);
-	};
-
-	const handleOpen = (note) => {
-		setValue(note);
-		setOpen(true);
-	};
 
 	return (
 		<>
@@ -76,7 +65,12 @@ const NotesList = ({
 							},
 							{
 								text: <DeleteIcon />,
-								onPress: () => handleOpen(note),
+								onPress: confirm(
+									() => handleNoteDelete(note.id, history), {
+										title: `Are you sure you want to delete "${getTitle(note.text)}"?`,
+										confirmationText: 'Delete',
+									},
+								),
 								autoClose: true,
 								style: {
 									backgroundColor: 'red',
@@ -97,14 +91,10 @@ const NotesList = ({
 			</List>
 
 			<ContextMenu
-				closestElement=".context-menu-select"
 				arrayOfObjects={notes}
-				handleRemoveClick={handleOpen}
-			/>
-			<DeleteConfirmationDialog
-				open={open}
-				onClose={handleClose}
-				value={value}
+				closestElement=".context-menu-select"
+				handleNoteDelete={handleNoteDelete}
+				history={history}
 			/>
 		</>
 	);
@@ -112,4 +102,4 @@ const NotesList = ({
 
 NotesList.propTypes = propTypes;
 
-export default NotesList;
+export default withConfirm(NotesList);
