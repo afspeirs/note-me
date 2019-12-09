025fab086403106c24fa904e2efe3984edbb550c
Andy Speirs <andy.speirs2@gmail.com>
2019-12-09 13:51:49 +0000

    App hooks refactor (#64)
    * Refactored App to use hooks
    
    * Removed comments

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { StylesProvider, ThemeProvider } from '@material-ui/styles';
@@ -11,76 +11,45 @@ import { StateProvider } from '../StateContext';
 import { auth, db, provider } from '../../firebase';
 import Routes from '../Routes';
 
-export default class App extends Component {
-	state = {
-		drawerOpen: false,
-		edit: false,
-		loading: true,
-		notes: [],
-		settings: {
-			sort: localStorage.getItem('changeSort') || 'date-asc',
-			darkTheme: JSON.parse(localStorage.getItem('changeDarkTheme')) || false,
-		},
-		swSnackbar: {},
-		updateAvailable: false,
-		user: null,
-	}
+const App = () => {
+	const [drawerOpen, setDrawerOpen] = useState(false);
+	const [edit, setEdit] = useState(false);
+	const [loading, setLoading] = useState(true);
+	const [notes, setNotes] = useState([]);
+	const [settings, setSettings] = useState({
+		sort: localStorage.getItem('changeSort') || 'date-asc',
+		darkTheme: JSON.parse(localStorage.getItem('changeDarkTheme')) || false,
+	});
+	const [snackbarContent, setSnackbarConent] = useState({});
+	const [updateAvailable, setUpdateAvailable] = useState(false);
+	const [user, setUser] = useState(null);
 
-	componentDidMount() {
-		auth.onAuthStateChanged((user) => {
-			if (user) {
-				db.collection(user.uid)
-					.onSnapshot((snapshot) => {
-						const notes = snapshot.docs.map(doc => doc.data());
-						this.setState({ loading: false, notes, user });
-					});
-			} else {
-				this.setState({ loading: false, notes: [] });
-			}
-		});
-
-		window.addEventListener('keydown', this.handleKeyDown);
-		window.addEventListener('swNewContentAvailable', this.swNewContentAvailable);
-		window.addEventListener('swContentCached', this.swContentCached);
-	}
-
-	componentWillUnmount() {
-		window.removeEventListener('keydown', this.handleKeyDown);
-		window.removeEventListener('swNewContentAvailable', this.swNewContentAvailable);
-		window.removeEventListener('swContentCached', this.swContentCached);
-	}
-
-	swNewContentAvailable = () => {
-		const swSnackbar = {
+	const swNewContentAvailable = () => {
+		setSnackbarConent({
 			onClose: () => window.location.reload(true),
 			secondaryText: 'Update',
 			text: 'A new version is available',
-		};
-		this.setState({
-			swSnackbar,
-			updateAvailable: true,
 		});
-	}
+		setUpdateAvailable(true);
+	};
 
-	swContentCached = () => {
-		const swSnackbar = {
+	const swContentCached = () => {
+		setSnackbarConent({
 			onClose: () => {},
 			secondaryText: null,
 			text: 'Caching complete! Now available offline',
-		};
-		this.setState({ swSnackbar });
+		});
 	};
 
-	swSnackbarReset = () => {
-		const swSnackbar = {
+	const swSnackbarReset = () => {
+		setSnackbarConent({
 			onClose: () => {},
 			secondaryText: null,
 			text: null,
-		};
-		this.setState({ swSnackbar });
+		});
 	};
 
-	reducer = (state, action) => {
+	const reducer = (state, action) => {
 		localStorage.setItem(action.type, action.value);
 		switch (action.type) {
 			case 'changeSort':
@@ -89,11 +58,9 @@ export default class App extends Component {
 					sort: action.value,
 				};
 			case 'changeDarkTheme':
-				this.setState({
-					settings: {
-						...state,
-						darkTheme: action.value,
-					},
+				setSettings({
+					...settings,
+					darkTheme: action.value,
 				});
 
 				return {
@@ -105,35 +72,30 @@ export default class App extends Component {
 		}
 	};
 
-	signOut = () => auth.signOut()
-		.then(() => this.setState({ user: null }));
+	const signOut = () => auth.signOut()
+		.then(() => setUser(null));
 
-	signIn = () => auth.signInWithPopup(provider)
-		.then(({ user }) => this.setState({ user }));
+	const signIn = () => auth.signInWithPopup(provider)
+		.then(({ user: authUser }) => setUser(authUser));
 
-	setDrawerOpen = value => this.setState({ drawerOpen: value });
-
-	setEdit = edit => this.setState({ edit });
-
-	handleKeyDown = (event) => {
+	const handleKeyDown = (event) => {
 		// CTRL + B = Toggle sidebar
 		if (event.ctrlKey && event.key === 'b') {
 			event.preventDefault();
-			this.setState(prevState => ({ drawerOpen: !prevState.drawerOpen }));
+			setDrawerOpen(prevState => !prevState);
 		}
 		// CTRL + E = Toggle edit
 		if (event.ctrlKey && event.key === 'e') {
 			event.preventDefault();
-			this.setState(prevState => ({ edit: !prevState.edit }));
+			setEdit(prevState => !prevState);
 		}
 		// Disable some keyboard shortcuts
 		if (event.ctrlKey && (event.key === 's' || event.key === 'p')) {
 			event.preventDefault();
 		}
-	}
+	};
 
-	handleNoteAdd = (history) => {
-		const { user, notes } = this.state;
+	const handleNoteAdd = (history) => {
 		const emptyNotes = notes.filter(el => el.text === '');
 
 		if (emptyNotes.length !== 0) {
@@ -148,14 +110,11 @@ export default class App extends Component {
 			};
 
 			notes.unshift(value);
-			this.setState({ notes });
-
 			newNote.set(value).then(() => history.push(`/note/${value.id}`));
 		}
 	};
 
-	handleNoteDelete = (id, history) => {
-		const { user, notes } = this.state;
+	const handleNoteDelete = (id, history) => {
 		const note = notes.find(item => item.id === id);
 
 		db.collection(user.uid)
@@ -165,14 +124,11 @@ export default class App extends Component {
 		if (note) {
 			const indexOfNote = notes.indexOf(note);
 			notes.splice(indexOfNote, 1);
-			this.setState({ notes });
-
 			history.replace('/');
 		}
 	};
 
-	handleNoteUpdate = (id, text) => {
-		const { user, notes } = this.state;
+	const handleNoteUpdate = (id, text) => {
 		const index = notes.findIndex(note => note.id === id);
 		const value = {
 			...notes[index],
@@ -185,74 +141,90 @@ export default class App extends Component {
 			.set(value);
 
 		notes[index] = value;
-		this.setState({ notes });
 	};
 
-	render() {
-		const {
-			drawerOpen,
-			edit,
-			loading,
-			notes,
-			settings,
-			swSnackbar,
-			updateAvailable,
-			user,
-		} = this.state;
-
-		const muiTheme = createMuiTheme({
-			...theme,
-			...{
-				palette: {
-					...theme.palette,
-					type: settings.darkTheme === true ? 'dark' : 'light',
-				},
+	const muiTheme = createMuiTheme({
+		...theme,
+		...{
+			palette: {
+				...theme.palette,
+				type: settings.darkTheme === true ? 'dark' : 'light',
 			},
+		},
+	});
+
+	useEffect(() => {
+		auth.onAuthStateChanged((authUser) => {
+			if (authUser) {
+				setUser(authUser);
+				db.collection(authUser.uid)
+					.onSnapshot((snapshot) => {
+						const authNotes = snapshot.docs.map(doc => doc.data());
+						setLoading(false);
+						setNotes(authNotes);
+					});
+			} else {
+				setLoading(false);
+				setNotes([]);
+				setUser(false);
+			}
 		});
 
-		return (
-			<BrowserRouter>
-				<ThemeProvider theme={muiTheme}>
-					<StylesProvider injectFirst>
-						<StateProvider initialState={settings} reducer={this.reducer}>
-							<Container
+		window.addEventListener('keydown', handleKeyDown);
+		window.addEventListener('swNewContentAvailable', swNewContentAvailable);
+		window.addEventListener('swContentCached', swContentCached);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+			window.removeEventListener('swNewContentAvailable', swNewContentAvailable);
+			window.removeEventListener('swContentCached', swContentCached);
+		};
+	}, []); // eslint-disable-line
+
+	return (
+		<BrowserRouter>
+			<ThemeProvider theme={muiTheme}>
+				<StylesProvider injectFirst>
+					<StateProvider initialState={settings} reducer={reducer}>
+						<Container
+							drawerOpen={drawerOpen}
+							edit={edit}
+							handleNoteAdd={handleNoteAdd}
+							handleNoteDelete={handleNoteDelete}
+							isSignedIn={user !== false}
+							loading={loading}
+							notes={notes}
+							setDrawerOpen={setDrawerOpen}
+							setEdit={setEdit}
+						>
+							<Routes
 								drawerOpen={drawerOpen}
 								edit={edit}
-								handleNoteAdd={this.handleNoteAdd}
-								handleNoteDelete={this.handleNoteDelete}
-								isSignedIn={Boolean(user)}
+								handleNoteDelete={handleNoteDelete}
+								handleNoteUpdate={handleNoteUpdate}
 								loading={loading}
 								notes={notes}
-								setDrawerOpen={this.setDrawerOpen}
-								setEdit={this.setEdit}
-							>
-								<Routes
-									drawerOpen={drawerOpen}
-									edit={edit}
-									handleNoteDelete={this.handleNoteDelete}
-									handleNoteUpdate={this.handleNoteUpdate}
-									loading={loading}
-									notes={notes}
-									setEdit={this.setEdit}
-									signIn={this.signIn}
-									signOut={this.signOut}
-									updateAvailable={updateAvailable}
-									user={user}
-								/>
-							</Container>
+								setEdit={setEdit}
+								signIn={signIn}
+								signOut={signOut}
+								updateAvailable={updateAvailable}
+								user={user}
+							/>
+						</Container>
 
-							{swSnackbar && (
-								<SimpleSnackbar
-									onClose={this.swSnackbarReset}
-									onSecondaryClose={swSnackbar.onClose}
-									secondaryText={swSnackbar.secondaryText}
-									text={swSnackbar.text}
-								/>
-							)}
-						</StateProvider>
-					</StylesProvider>
-				</ThemeProvider>
-			</BrowserRouter>
-		);
-	}
-}
+						{snackbarContent && (
+							<SimpleSnackbar
+								onClose={swSnackbarReset}
+								onSecondaryClose={snackbarContent.onClose}
+								secondaryText={snackbarContent.secondaryText}
+								text={snackbarContent.text}
+							/>
+						)}
+					</StateProvider>
+				</StylesProvider>
+			</ThemeProvider>
+		</BrowserRouter>
+	);
+};
+
+export default App;
