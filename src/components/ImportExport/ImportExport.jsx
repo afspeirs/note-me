67a0d7095033e80856aa1e1e8cce3e06211b82f2
Andy <andy.speirs2@gmail.com>
2020-02-17 17:31:04 +0000

    Add export note functionality

--- src/components/ImportExport/ImportExport.jsx
+++ src/components/ImportExport/ImportExport.jsx
@@ -7,11 +7,12 @@ import {
 } from '@material-ui/core';
 
 import useStyles from './ImportExport.styled';
+import { getTitle } from '../../ultils';
 import { useNotes } from '../../hooks/NotesContext';
 
 const ImportExport = () => {
 	const classes = useStyles();
-	const { handleNoteAdd } = useNotes();
+	const { handleNoteAdd, notes } = useNotes();
 
 	// Read each file and create a note for it
 	const onChangeImport = (event) => [...event.target.files].forEach((file) => {
@@ -20,25 +21,56 @@ const ImportExport = () => {
 		reader.readAsText(file);
 	});
 
+	const exportMarkdownFile = (name, text) => {
+		const element = document.createElement('a');
+
+		element.setAttribute('href', `data:text/plain;charset=utf-8,${encodeURIComponent(text)}`);
+		element.setAttribute('download', `${name}.md`);
+		element.style.display = 'none';
+		document.body.appendChild(element);
+		element.click();
+		document.body.removeChild(element);
+	};
+
+	const onClickExport = () => notes.forEach((note) => {
+		const title = getTitle(note.text);
+		const { text } = note;
+		exportMarkdownFile(title, text);
+	});
+
 	return (
-		<ListItem>
-			<ListItemText primary="Import Notes:" />
-			<ListItemSecondaryAction>
-				<input
-					accept="text/*"
-					className={classes.input}
-					id="import-note"
-					multiple
-					onChange={onChangeImport}
-					type="file"
-				/>
-				<label htmlFor="import-note">
-					<Button color="primary" component="span" variant="contained">
-						Import
+		<>
+			<ListItem>
+				<ListItemText primary="Import Notes:" />
+				<ListItemSecondaryAction>
+					<label htmlFor="import-note">
+						<input
+							accept="text/*"
+							className={classes.input}
+							id="import-note"
+							multiple
+							onChange={onChangeImport}
+							type="file"
+						/>
+						<Button color="primary" component="span" variant="contained">
+							Import
+						</Button>
+					</label>
+				</ListItemSecondaryAction>
+			</ListItem>
+			<ListItem>
+				<ListItemText primary="Export Notes:" />
+				<ListItemSecondaryAction>
+					<Button
+						color="primary"
+						variant="contained"
+						onClick={onClickExport}
+					>
+						Export
 					</Button>
-				</label>
-			</ListItemSecondaryAction>
-		</ListItem>
+				</ListItemSecondaryAction>
+			</ListItem>
+		</>
 	);
 };
 
