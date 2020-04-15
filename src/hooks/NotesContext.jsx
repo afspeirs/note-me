1dc3d3d11b9873af00813a916b2125e8b7469528
Andy <andy.speirs2@gmail.com>
2020-04-15 21:19:57 +0100

    Updated addNote function

--- src/hooks/NotesContext.jsx
+++ src/hooks/NotesContext.jsx
@@ -23,7 +23,7 @@ function useNotesProvider() {
 	const [notes, setNotes] = useState([]);
 	const [currentNote, setCurrentNote] = useState(null);
 
-	const addNote = (text = '') => {
+	const addNote = (text) => {
 		const untitledNotes = notes.filter((note) => note.text === '');
 
 		if (untitledNotes.length !== 0) {
