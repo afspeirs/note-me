06436cd48941f09dd08e0e3b03f51584f1422491
Andy <andy.speirs2@gmail.com>
2020-10-17 12:08:22 +0100

    Added Hide add to homescreen snackbar

--- src/components/CheckForInstallPrompt/CheckForInstallPrompt.jsx
+++ src/components/CheckForInstallPrompt/CheckForInstallPrompt.jsx
@@ -7,9 +7,11 @@ import {
 } from '@material-ui/core';
 
 import { useGlobalState } from '../../hooks/GlobalState';
+import { useSnackbar } from '../../hooks/Snackbar';
 
 const AddToHomescreen = () => {
 	const [{ beforeInstallPrompt }, dispatch] = useGlobalState();
+	const snackbar = useSnackbar();
 
 	const handleButtonClick = async () => {
 		beforeInstallPrompt.prompt();
@@ -21,6 +23,17 @@ const AddToHomescreen = () => {
 				value: false,
 			});
 		}
+
+		snackbar.showMessage({
+			message: 'Hide "Add to Homescreen" in Settings',
+			actionText: 'Hide',
+			actionFunction() {
+				dispatch({
+					type: 'app-beforeInstallPrompt',
+					value: false,
+				});
+			},
+		});
 	};
 
 	return Boolean(beforeInstallPrompt) && (
@@ -32,7 +45,7 @@ const AddToHomescreen = () => {
 					color="primary"
 					onClick={handleButtonClick}
 				>
-					Install
+					Add
 				</Button>
 			</ListItemSecondaryAction>
 		</ListItem>
