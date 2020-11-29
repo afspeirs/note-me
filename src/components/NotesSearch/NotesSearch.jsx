d285b8d23f2305f5072bf792cac067a6f1a87ac9
AFSpeirs <andy.speirs2@gmail.com>
2020-11-29 15:15:57 +0000

    Updated styling of Search

--- src/components/NotesSearch/NotesSearch.jsx
+++ src/components/NotesSearch/NotesSearch.jsx
@@ -49,7 +49,7 @@ const NotesSearch = () => {
 			{/* This div makes the Transitions work */}
 			<div>
 				<Slide in={search.show} direction="left" timeout={256} mountOnEnter unmountOnExit>
-					<AppBar position="fixed">
+					<AppBar position="absolute" elevation={0}>
 						<Toolbar>
 							<IconButton
 								className={classes.menuButton}
@@ -60,30 +60,28 @@ const NotesSearch = () => {
 							>
 								<ArrowBackIcon />
 							</IconButton>
-							<div className={classes.search}>
-								<InputBase
-									autoFocus
-									classes={{
-										root: classes.inputRoot,
-										input: classes.inputInput,
-									}}
-									inputProps={{ 'aria-label': 'search' }}
-									onChange={handleTextInput}
-									placeholder="Search Notes"
-									value={search.text}
-									endAdornment={search.text.length !== 0 && (
-										<IconButton
-											aria-label="Clear Search"
-											className={classes.searchClear}
-											color="inherit"
-											onClick={handleTextClear}
-											size="small"
-										>
-											<ClearIcon />
-										</IconButton>
-									)}
-								/>
-							</div>
+							<InputBase
+								autoFocus
+								className={classes.search}
+								classes={{
+									root: classes.inputRoot,
+									input: classes.inputInput,
+								}}
+								inputProps={{ 'aria-label': 'search' }}
+								onChange={handleTextInput}
+								placeholder="Search Notes"
+								value={search.text}
+								endAdornment={search.text.length !== 0 && (
+									<IconButton
+										aria-label="Clear Search"
+										edge="end"
+										color="inherit"
+										onClick={handleTextClear}
+									>
+										<ClearIcon />
+									</IconButton>
+								)}
+							/>
 						</Toolbar>
 					</AppBar>
 				</Slide>
--- src/components/NotesSearch/NotesSearch.styled.js
+++ src/components/NotesSearch/NotesSearch.styled.js
@@ -1,4 +1,4 @@
-import { fade, makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
 	menuButton: {
@@ -6,22 +6,9 @@ const useStyles = makeStyles((theme) => ({
 	},
 	search: {
 		position: 'relative',
+		flexGrow: 1,
 		width: '100%',
-		borderRadius: theme.shape.borderRadius,
-		backgroundColor: fade(theme.palette.common.black, 0.10),
-		'&:hover': {
-			backgroundColor: fade(theme.palette.common.black, 0.15),
-		},
-	},
-	searchClear: {
-		marginRight: theme.spacing(0.5),
-		marginLeft: -theme.spacing(0.5),
-	},
-	searchIcon: {
-		position: 'absolute',
-		right: 0,
-		width: theme.spacing(5),
-		pointerEvents: 'none',
+		fontSize: '1.25rem',
 	},
 	inputRoot: {
 		color: 'inherit',
