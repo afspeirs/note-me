a2be44604a45c78488935834197b9e40d4dfc48d
Andy Speirs <andy.speirs2@gmail.com>
2020-11-29 15:49:05 +0000

    Added FAB to add a note to HomePage (#125)

--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
 	centered: {
 		display: 'flex',
 		alignItems: 'center',
@@ -8,12 +8,17 @@ const useStyles = makeStyles({
 		width: '100%',
 		height: '100%',
 	},
+	fab: {
+		position: 'fixed',
+		bottom: theme.spacing(2),
+		right: theme.spacing(2),
+	},
 	page: {
 		display: 'flex',
 		flexGrow: 1,
 		overflowY: 'auto',
 		overflowX: 'hidden',
 	},
-});
+}));
 
 export default useStyles;
