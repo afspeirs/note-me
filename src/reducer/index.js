f87c6e1b3962b4170b412ecff536f8f96857ff8c
AFSpeirs <andy.speirs2@gmail.com>
2021-03-12 18:10:38 +0000

    feat: updated logic of reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -4,14 +4,14 @@ export const reducer = (state, action) => {
 	const [location, name] = action.type.split('-');
 
 	if (location === 'settings') {
-		const value = action.value !== undefined ? action.value : !state.settings[name];
+		const value = action.value || !state[location][name];
 
-		localStorage.setItem(action.type, value);
+		localStorage.setItem(action.type, JSON.stringify(value));
 
 		return {
 			...state,
-			settings: {
-				...state.settings,
+			[location]: {
+				...state[location],
 				[name]: value,
 			},
 		};
