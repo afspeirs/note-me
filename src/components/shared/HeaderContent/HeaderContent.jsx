f407c50b7da6183f8c8492a211adb65c7182c81e
AFSpeirs <andy.speirs2@gmail.com>
2021-05-31 14:51:08 +0100

    feat: only show first header item in mobile and show the rest in a menu

--- src/components/shared/HeaderContent/HeaderContent.jsx
+++ src/components/shared/HeaderContent/HeaderContent.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
 	IconButton,
+	ListItemIcon,
+	Menu,
+	MenuItem,
 	Tooltip,
 	useMediaQuery,
 } from '@material-ui/core';
+import {
+	MoreVert as MoreIcon,
+} from '@material-ui/icons';
 
 const defaultProps = {
 	forceLastIconEdge: false,
@@ -24,27 +31,104 @@ const propTypes = {
 
 const HeaderContent = ({ forceLastIconEdge, headerItems }) => {
 	const mobile = useMediaQuery('(max-width:600px)');
+	const [anchorEl, setAnchorEl] = useState(null);
+	const handleClick = (event) => setAnchorEl(event.currentTarget);
+	const handleClose = () => setAnchorEl(null);
+	const [first, ...rest] = headerItems;
 
 	return (
 		<>
-			{headerItems.map((item, index) => (
-				<Tooltip key={item.text} title={item.text}>
-					{/* Wrapper element in case the Button is disabled */}
-					<span>
-						<IconButton
-							aria-label={item.text}
-							color="inherit"
-							component={item.component}
-							edge={((forceLastIconEdge || mobile) && index === headerItems.length - 1) && 'end'}
-							onClick={item.onClick}
-							to={item.to}
-							disabled={item.disabled}
-						>
-							{item.icon}
-						</IconButton>
-					</span>
-				</Tooltip>
-			))}
+			{mobile && first ? (
+				<>
+					<Tooltip key={first.text} title={first.text}>
+						{/* Wrapper element in case the Button is disabled */}
+						<span>
+							<IconButton
+								aria-label={first.text}
+								color="inherit"
+								component={first.component}
+								disabled={first.disabled}
+								edge={((forceLastIconEdge || mobile) && rest.length === 0) ? 'end' : 'false'}
+								onClick={first.onClick}
+								to={first.to}
+							>
+								{first.icon}
+							</IconButton>
+						</span>
+					</Tooltip>
+
+					{rest.length !== 0 && (
+						<>
+							<Tooltip title="Show More">
+								<IconButton
+									aria-controls="more-menu"
+									aria-haspopup="true"
+									aria-label="Show more"
+									color="inherit"
+									edge="end"
+									onClick={handleClick}
+								>
+									<MoreIcon />
+								</IconButton>
+							</Tooltip>
+
+							<Menu
+								id="more-menu"
+								anchorEl={anchorEl}
+								keepMounted
+								open={Boolean(anchorEl)}
+								onClose={handleClose}
+							>
+								{rest.map((item, index) => (
+									<MenuItem
+										key={item.text}
+										aria-label={item.text}
+										color="inherit"
+										component={item.component}
+										disabled={item.disabled}
+										edge={((forceLastIconEdge || mobile) && index === headerItems.length - 1) ? 'end' : 'false'}
+										onClick={() => {
+											handleClose();
+											item.onClick();
+										}}
+										to={item.to}
+									>
+										<ListItemIcon
+											color="inherit"
+											aria-label="Create Note"
+											edge="start"
+										>
+											{item.icon}
+										</ListItemIcon>
+										<span>{item.text}</span>
+									</MenuItem>
+								))}
+							</Menu>
+						</>
+					)}
+				</>
+			) : (
+				<>
+					{headerItems.map((item, index) => (
+						<Tooltip key={item.text} title={item.text}>
+							{/* Wrapper element in case the Button is disabled */}
+							<span>
+								<IconButton
+									aria-label={item.text}
+									color="inherit"
+									component={item.component}
+									disabled={item.disabled}
+									edge={((forceLastIconEdge || mobile) && index === headerItems.length - 1) ? 'end' : 'false'}
+									onClick={item.onClick}
+									to={item.to}
+								>
+									{item.icon}
+								</IconButton>
+							</span>
+						</Tooltip>
+					))}
+				</>
+			)}
 		</>
 	);
 };
