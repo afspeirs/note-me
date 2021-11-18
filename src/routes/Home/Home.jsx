a3c1ed6a52e35e8bdbb7418b04ec443dd6fd2adf
AFSpeirs <andy.speirs2@gmail.com>
2021-11-18 13:43:54 +0000

    fix: updated how the copy loads based on when the user loads

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -9,16 +9,13 @@ import {
 
 import Page from '@/components/shared/Page';
 import { useAuth } from '@/hooks/Auth';
-import { useNotes } from '@/hooks/Notes';
 
 const Home = () => {
 	const { signIn, user } = useAuth();
-	const { loading } = useNotes();
 
 	return (
 		<Page>
 			<List>
-				{/* eslint-disable max-len */}
 				<ListItem>
 					<ListItemText primary={`Hello and welcome to ${import.meta.env.VITE_APP_TITLE}`} />
 				</ListItem>
@@ -38,24 +35,32 @@ const Home = () => {
 					</Typography>
 				</ListItem>
 
-				{!user === null && !loading ? (
-					<>
-						<ListItem>
-							<ListItemText primary="Once signed in you can access your notes from any device, and changes will be reflected across other devices seamlessly." />
-						</ListItem>
-						{/* eslint-enable max-len */}
-
-						<ListItem>
-							<Button variant="contained" color="primary" onClick={signIn}>
-								Sign in with Google
-							</Button>
-						</ListItem>
-					</>
-				) : (
+				{user === null ? (
 					<ListItem>
-						<ListItemText primary="Select a note from the left side to get started" />
+						<ListItemText primary="Loading..." />
 					</ListItem>
+				) : (
+					<>
+						{user === false ? (
+							<>
+								<ListItem>
+									<ListItemText primary="Once signed in you can access your notes from any device, and changes will be reflected across other devices seamlessly." />
+								</ListItem>
+
+								<ListItem>
+									<Button variant="contained" color="primary" onClick={signIn}>
+										Sign in with Google
+									</Button>
+								</ListItem>
+							</>
+						) : (
+							<ListItem>
+								<ListItemText primary="Select a note from the left side to get started" />
+							</ListItem>
+						)}
+					</>
 				)}
+
 			</List>
 		</Page>
 	);
