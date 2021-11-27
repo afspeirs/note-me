e617c6d0d130ebf63e485c07382e23436175cdcc
AFSpeirs <andy.speirs2@gmail.com>
2021-11-27 15:12:21 +0000

    feat: Add support for Modal Pages again feat: Made Settings Page a Modal

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,7 +1,9 @@
+import { useEffect } from 'react';
 import {
 	Redirect,
 	Route,
 	Switch,
+	useLocation,
 } from 'react-router-dom';
 
 import { useAuth } from '@/hooks/Auth';
@@ -10,19 +12,45 @@ import Home from '@/routes/Home';
 import Note from '@/routes/Note';
 import Settings from '@/routes/Settings';
 
+/**
+ * Update this array with the routes that can be opened as modals
+ */
+export const arrayOfModals = [
+	'/settings/',
+];
+
 const Routes = () => {
 	const { user } = useAuth();
+	const location = useLocation();
+	const isModal = arrayOfModals.findIndex((path) => location.pathname.startsWith(path)) !== -1;
+
+	// Save window.previousLocation to sessionStorage for later
+	// If the page reloads with a modal open it renders the 404 page instead of the HomePage
+	useEffect(() => {
+		if (window.previousLocation) {
+			sessionStorage.setItem('previousLocation', JSON.stringify(window.previousLocation));
+		} else {
+			window.previousLocation = JSON.parse(sessionStorage.getItem('previousLocation'));
+		}
+	}, []);
+
+	useEffect(() => {
+		if (!isModal) {
+			window.previousLocation = location;
+		}
+	}, [location]);
 
 	return (
 		<>
-			<Switch>
+			<Switch location={isModal ? window.previousLocation : location}>
 				<Route path="/create-note/" component={CreateNote} />
 				<Route path="/note/:id" component={Note} />
-				<Route path="/settings/" component={Settings} />
 				<Route path="/" component={Home} />
 			</Switch>
 
 			{user === false && <Redirect to="/" />}
+
+			<Route path="/settings/" component={Settings} />
 		</>
 	);
 };
