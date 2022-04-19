7d9a53dc94c5f72bf1af7c41792528080704bdb7
AFSpeirs <andy.speirs2@gmail.com>
2022-04-19 20:51:40 +0100

    refactor: update path import

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import path from 'path';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import loadVersion from 'vite-plugin-package-version';
@@ -11,7 +11,7 @@ export default defineConfig({
 	],
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, './src'),
+			'@': resolve(__dirname, './src'),
 		},
 	},
 });
