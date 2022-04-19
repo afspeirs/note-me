881a888c05cf08e052c46a01dfb3a57621ccfc2f
AFSpeirs <andy.speirs2@gmail.com>
2022-04-19 20:53:52 +0100

    refactor: update packageVersion variable name

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import loadVersion from 'vite-plugin-package-version';
+import packageVersion from 'vite-plugin-package-version';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		loadVersion(),
+		packageVersion(),
 		react(),
 	],
 	resolve: {
