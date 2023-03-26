423f3ab5158218c098a9c4776764b21e94927a2d
Andy <andy@speirs.dev>
2023-03-26 20:23:21 +0100

    feat: add new transition to drawer

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,34 +1,42 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Transition } from '@headlessui/react';
 
 import { Sidebar } from '../components/Sidebar';
 import { classNames } from '../utils/classNames';
 
 export function Layout() {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="absolute inset-0 flex overflow-hidden">
-      <aside
-        aria-hidden={!open}
-        className={classNames(
-          'flex flex-col mx-2 mb-2 mt-2 gap-2 min-w-80 transition-transform',
-          open ? '' : '-translate-x-[21rem] sm:-translate-x-[20.5rem]',
-        )}
+      <Transition
+        as="aside"
+        show={open}
+        unmount={false}
+        enter="transition-opacity duration-200"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        className="absolute w-80 p-2 h-full flex flex-col gap-2"
       >
         <Sidebar />
-      </aside>
-      <main
+      </Transition>
+      <div
         className={classNames(
-          'flex-1 mt-2 min-w-full sm:min-w-[initial] bg-white dark:bg-black dark:text-white rounded-t-xl transition-[margin-left] mr-0 sm:mr-2 overflow-y-auto shadow',
-          open ? 'ml-0' : '-ml-[21rem] sm:-ml-[20.5rem]',
+          'flex-1 px-2 pt-2 min-w-full sm:min-w-[initial] transition-[margin-left] duration-200',
+          open ? 'ml-[19.5rem]' : '',
         )}
       >
-        <nav className="p-4">
-          <button type="button" onClick={() => setOpen((prevState) => !prevState)}>{open ? 'CLOSE' : 'OPEN'}</button>
-        </nav>
-        <Outlet />
-      </main>
+        <main className="h-full bg-white dark:bg-black dark:text-white rounded-t-xl overflow-y-auto shadow">
+          <nav className="p-4">
+            <button type="button" onClick={() => setOpen((prevState) => !prevState)}>{open ? 'CLOSE' : 'OPEN'}</button>
+          </nav>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
