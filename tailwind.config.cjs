ddf1f70076715ca6e26c53fbd4f57be424896288
Andy <andy@speirs.dev>
2023-03-26 18:20:42 +0100

    chore: update to framework

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,15 @@
-const {
-  black,
-  white,
-} = require('tailwindcss/colors');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/**/*.{html,ts,tsx}',
   ],
   theme: {
-    colors: {
-      primary: '#ee6e00',
-      black,
-      white,
-    },
     extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        primary: '#ee6e00',
+      },
       boxShadow: {
         DEFAULT: '-1px 1px 4px 0px rgba(0, 0, 0, 0.4)',
       },
