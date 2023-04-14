e39bd622c4b504bb8d99cb8b8f9b207ba0f5a4e5
Andy <andy@speirs.dev>
2023-04-14 19:40:06 +0100

    feat: update ButtonIcon to support links

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,12 +1,26 @@
+import { NavLink } from 'react-router-dom';
+
+import { classNames } from '../../utils/classNames';
 import type { ButtonIconProps } from './types';
 
 export function ButtonIcon({
   className,
+  href,
   Icon,
   label,
   onClick,
 }: ButtonIconProps) {
-  return (
+  return href ? (
+    <NavLink
+      className={({ isActive }) => classNames(
+        'block px-4 py-2',
+        isActive ? 'text-neutral-500' : 'text-primary',
+      )}
+      to={href}
+    >
+      <Icon className="h-6 w-6 " />
+    </NavLink>
+  ) : (
     <button
       type="button"
       aria-label={label}
--- src/components/ButtonIcon/types.ts
+++ src/components/ButtonIcon/types.ts
@@ -2,7 +2,8 @@ import { HomeIcon } from '@heroicons/react/24/outline';
 
 export interface ButtonIconProps {
   className?: string;
+  href?: string,
   Icon: typeof HomeIcon,
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
 }
