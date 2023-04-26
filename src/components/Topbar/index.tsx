453c24f0a1682972133a1f28d618ee3e9b8c428f
Andy <andy@speirs.dev>
2023-04-26 21:54:17 +0100

    fix: update text colour

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,7 +1,7 @@
 export function Topbar() {
   return (
     <div className="fixed pwa-header drag hidden [@media(display-mode:window-controls-overlay)]:flex items-center px-4">
-      <span className="text-sm">
+      <span className="text-sm text-white">
         NoteMe
       </span>
     </div>
