a9e59d3c213a6870e27ce6bc6f4ca681f4e9962a
Andy <andy@speirs.dev>
2023-04-26 22:14:51 +0100

    feat: update dark mode colours

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -6,10 +6,10 @@ import { SidebarHeader } from '../SidebarHeader';
 export function Sidebar() {
   return (
     <>
-      <div className="bg-white dark:bg-black dark:text-white rounded-xl shadow">
+      <div className="bg-white dark:bg-neutral-800 dark:text-white rounded-xl shadow">
         <SidebarHeader />
       </div>
-      <div className="flex-1 bg-white dark:bg-black dark:text-white rounded-xl overflow-hidden shadow">
+      <div className="flex-1 bg-white dark:bg-neutral-800 dark:text-white rounded-xl overflow-hidden shadow">
         <NavLink
           className={({ isActive }) => classNames(
             'block px-4 py-2',
--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 import { Transition } from '@headlessui/react';
 import { useAtom } from 'jotai';
 
@@ -7,7 +8,6 @@ import { Sidebar } from '../components/Sidebar';
 import { Topbar } from '../components/Topbar';
 import { drawerOpenAtom } from '../context/navigation';
 import { themeAtom } from '../context/theme';
-import { classNames } from '../utils/classNames';
 
 export function Layout() {
   const [drawerOpen, setDrawerOpen] = useAtom(drawerOpenAtom);
@@ -21,50 +21,54 @@ export function Layout() {
   }, [theme]);
 
   return (
-    <div
-      className={classNames(
-        'absolute inset-0 flex overflow-hidden mt-[env(titlebar-area-height)]',
-        appTheme,
-      )}
-    >
-      <Topbar />
-      <Transition show={drawerOpen}>
-        <Transition.Child
-          as="aside"
-          unmount={false}
-          enter="transition-transform duration-400"
-          enterFrom="-translate-x-[19.75rem]"
-          enterTo="translate-x-0"
-          leave="transition-transform duration-400"
-          leaveFrom="translate-x-0"
-          leaveTo="-translate-x-[19.75rem]"
-          className="absolute w-80 p-1 h-full flex flex-col gap-1"
-        >
-          <Sidebar />
-        </Transition.Child>
-        <Transition.Child
-          role="presentation"
-          enter="transition-[margin-left] duration-400"
-          enterFrom="ml-0"
-          enterTo="ml-[19.75rem]"
-          leave="transition-[margin-left] duration-400"
-          leaveFrom="ml-[19.75rem]"
-          leaveTo="scale-x-100"
-        />
-      </Transition>
-      <div className="relative flex-1 px-1 pt-1 min-w-full sm:min-w-[initial]">
-        <button
-          type="button"
-          className="absolute inset-0 mx-1 mt-1 disabled:hidden sm:hidden rounded-t-xl z-10"
-          disabled={!drawerOpen}
-          onClick={() => setDrawerOpen((prevState) => !prevState)}
-        >
-          <span className="sr-only">Hide Sidebar</span>
-        </button>
-        <div className="flex flex-col h-full bg-white dark:bg-black dark:text-white rounded-t-xl overflow-hidden shadow">
-          <Outlet />
+    <>
+      <Helmet>
+        <meta name="theme-color" content={appTheme === 'light' ? '#ee6e00' : '#000000'} />
+        <body className={appTheme} />
+      </Helmet>
+
+      <div
+        className="absolute inset-0 flex overflow-hidden mt-[env(titlebar-area-height)] bg-primary dark:bg-black"
+      >
+        <Topbar />
+        <Transition show={drawerOpen}>
+          <Transition.Child
+            as="aside"
+            unmount={false}
+            enter="transition-transform duration-400"
+            enterFrom="-translate-x-[19.75rem]"
+            enterTo="translate-x-0"
+            leave="transition-transform duration-400"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-[19.75rem]"
+            className="absolute w-80 p-1 h-full flex flex-col gap-1"
+          >
+            <Sidebar />
+          </Transition.Child>
+          <Transition.Child
+            role="presentation"
+            enter="transition-[margin-left] duration-400"
+            enterFrom="ml-0"
+            enterTo="ml-[19.75rem]"
+            leave="transition-[margin-left] duration-400"
+            leaveFrom="ml-[19.75rem]"
+            leaveTo="scale-x-100"
+          />
+        </Transition>
+        <div className="relative flex-1 px-1 pt-1 min-w-full sm:min-w-[initial]">
+          <button
+            type="button"
+            className="absolute inset-0 mx-1 mt-1 disabled:hidden sm:hidden rounded-t-xl z-10"
+            disabled={!drawerOpen}
+            onClick={() => setDrawerOpen((prevState) => !prevState)}
+          >
+            <span className="sr-only">Hide Sidebar</span>
+          </button>
+          <div className="flex flex-col h-full bg-white dark:bg-neutral-800 dark:text-white rounded-t-xl overflow-hidden shadow">
+            <Outlet />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
