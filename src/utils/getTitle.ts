5b171d3a42b5b7de2312d86fce0be9eb5ffed434
Andy <andy@speirs.dev>
2023-06-28 23:27:06 +0100

    feat: update getTitle to cap the length to 35 characters

--- src/utils/getTitle.ts
+++ src/utils/getTitle.ts
@@ -1,6 +1,20 @@
 import type { NoteDocument } from '../api/types';
 
+const maxCharacters = 35;
+
 export function getTitle(note: NoteDocument) {
-  const title = note.text.split('\n')[0].replace(/#+ /g, '');
-  return title.length > 0 ? title : 'Untitled';
+  const title = note.text
+    .trim()
+    .split('\n')[0]
+    .replace(/#+ /g, '');
+
+  if (title.length > 0) {
+    if (title.length > maxCharacters) {
+      return title
+        .substring(0, maxCharacters - 3)
+        .padEnd(maxCharacters, '.');
+    }
+    return title;
+  }
+  return 'Untitled';
 }
