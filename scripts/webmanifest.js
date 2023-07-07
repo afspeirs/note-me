b7b5946541e7d644dbf67ff0a7be1ce3a5417ea1
andy.speirs <andy.speirs@havas.com>
2023-07-07 09:21:38 +0100

    add error message

--- scripts/webmanifest.js
+++ scripts/webmanifest.js
@@ -12,5 +12,9 @@ fs.readFile('./dist/manifest.webmanifest', 'utf8', (err, data) => {
 
   console.log(webmanifest);
 
-  fs.writeFile('./dist/manifest.webmanifest', JSON.stringify(webmanifest, null, 2));
+  fs.writeFile('./dist/manifest.webmanifest', JSON.stringify(webmanifest, null, 2), (error) => {
+    if (error) {
+      console.log('An error has occurred ', error); // eslint-disable-line no-console
+    }
+  });
 });
