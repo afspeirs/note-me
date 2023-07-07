bf87bfe6163dbcd308c7d8e8943421e4728aa526
andy.speirs <andy.speirs@havas.com>
2023-07-07 09:14:04 +0100

    log process.env

--- scripts/webmanifest.js
+++ scripts/webmanifest.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 fs.readFile('./dist/manifest.webmanifest', 'utf8', (err, data) => {
   const webmanifest = JSON.parse(data);
 
-  console.log(process.env.BRANCH);
+  console.log(process.env);
 
   if (process.env.BRANCH) {
     webmanifest.name += ` (${process.env.BRANCH})`;
