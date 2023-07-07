7c6a65a9eeafb2ec898a3dd7d388d5fa36f8a6c5
andy.speirs <andy.speirs@havas.com>
2023-07-07 09:19:30 +0100

    write file back

--- scripts/webmanifest.js
+++ scripts/webmanifest.js
@@ -2,12 +2,15 @@ import fs from 'fs';
 
 fs.readFile('./dist/manifest.webmanifest', 'utf8', (err, data) => {
   const webmanifest = JSON.parse(data);
+  const branchName = process.env.HEAD;
 
   console.log(process.env);
 
-  if (process.env.BRANCH) {
-    webmanifest.name += ` (${process.env.BRANCH})`;
+  if (branchName) {
+    webmanifest.name += ` (${branchName})`;
   }
 
   console.log(webmanifest);
+
+  fs.writeFile('./dist/manifest.webmanifest', JSON.stringify(webmanifest, null, 2));
 });
