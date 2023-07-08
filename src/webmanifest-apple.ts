7f89a1c2348443a208c0838a4baa4c1d7bcd65aa
andy.speirs <andy.speirs@havas.com>
2023-07-08 16:41:33 +0100

    fix: only update webmanifest url in production

--- src/webmanifest-apple.ts
+++ src/webmanifest-apple.ts
@@ -1,5 +1,5 @@
 const webmanifest = document.getElementById('webmanifest') as HTMLLinkElement;
 
-if (navigator.userAgent.search('Mac') !== -1 && webmanifest) {
+if (import.meta.env.PROD && navigator.userAgent.search('Mac') !== -1 && webmanifest) {
   webmanifest.href = '/manifest-apple.webmanifest';
 }
