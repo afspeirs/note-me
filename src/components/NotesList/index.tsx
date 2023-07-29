d7553a09fb705413757369faf06b2f257d13c82f
Andy <andy@speirs.dev>
2023-07-29 17:51:12 +0100

    refactor: move contents into NotesList component

---
+++ src/components/NotesList/index.tsx
@@ -0,0 +1,53 @@
+import type { NoteDocType } from '@/api/types';
+import { Button } from '@/components/Button';
+import { notesSearchAtom } from '@/context/notesSearch';
+import { notesSortAtom, notesSortOptions } from '@/context/notesSort';
+import { getTitle } from '@/utils/getTitle';
+import { StarIcon as StarSolidIcon } from '@heroicons/react/24/solid';
+import { useAtomValue } from 'jotai';
+import { useRxData } from 'rxdb-hooks';
+import { NotesContextMenu } from './NotesContextMenu';
+
+export function NotesList() {
+  const search = useAtomValue(notesSearchAtom);
+  const sort = useAtomValue(notesSortAtom);
+  const { result: notes, isFetching } = useRxData<NoteDocType>(
+    'notes',
+    (collection) => collection.find({
+      selector: {
+        text: {
+          $regex: RegExp(search, 'i'),
+        },
+      },
+      sort: notesSortOptions[sort].value,
+    }),
+  );
+
+  return (
+    <ul role="list" className="flex flex-col p-2 gap-1 overflow-y-auto h-full">
+      {isFetching && (
+        <li className="block p-3">Loading...</li>
+      )}
+      {!isFetching && notes.length === 0 && (
+        <li className="block p-3">No notes found</li>
+      )}
+      {notes.map((note) => (
+        <li key={note.id} className="group relative flex">
+          <Button
+            className="group-hover:pr-14"
+            href={`/note/${note.id}`}
+            secondaryAction={note.favourite && (
+              <StarSolidIcon className="h-6 w-6 flex-shrink-0 text-primary" aria-hidden="true" />
+            )}
+          >
+            {getTitle(note.text)}
+          </Button>
+          {/* TODO: Have this styled like CheckForInstallPrompt */}
+          <div className="absolute right-0">
+            <NotesContextMenu note={note} />
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- src/components/Sidebar/Content.tsx
+++ src/components/Sidebar/Content.tsx
@@ -1,56 +1,10 @@
-import type { NoteDocType } from '@/api/types';
-import { Button } from '@/components/Button';
-import { Card } from '@/components/Card';
-import { notesSearchAtom } from '@/context/notesSearch';
-import { notesSortAtom, notesSortOptions } from '@/context/notesSort';
-import { getTitle } from '@/utils/getTitle';
-import { StarIcon as StarSolidIcon } from '@heroicons/react/24/solid';
-import { useAtomValue } from 'jotai';
-import { useRxData } from 'rxdb-hooks';
-import { NotesContextMenu } from '../NotesList/NotesContextMenu';
+import { Card } from '../Card';
+import { NotesList } from '../NotesList';
 
 export function Content() {
-  const search = useAtomValue(notesSearchAtom);
-  const sort = useAtomValue(notesSortAtom);
-  const { result: notes, isFetching } = useRxData<NoteDocType>(
-    'notes',
-    (collection) => collection.find({
-      selector: {
-        text: {
-          $regex: RegExp(search, 'i'),
-        },
-      },
-      sort: notesSortOptions[sort].value,
-    }),
-  );
-
   return (
     <Card as="nav" className="flex-1 h-full overflow-hidden" aria-label="Sidebar">
-      <ul role="list" className="flex flex-col p-2 gap-1 overflow-y-auto h-full">
-        {isFetching && (
-          <li className="block p-3">Loading...</li>
-        )}
-        {!isFetching && notes.length === 0 && (
-          <li className="block p-3">No notes found</li>
-        )}
-        {notes.map((note) => (
-          <li key={note.id} className="group relative flex">
-            <Button
-              className="group-hover:pr-14"
-              href={`/note/${note.id}`}
-              secondaryAction={note.favourite && (
-                <StarSolidIcon className="h-6 w-6 flex-shrink-0 text-primary" aria-hidden="true" />
-              )}
-            >
-              {getTitle(note.text)}
-            </Button>
-            {/* TODO: Have this styled like CheckForInstallPrompt */}
-            <div className="absolute right-0">
-              <NotesContextMenu note={note} />
-            </div>
-          </li>
-        ))}
-      </ul>
+      <NotesList />
     </Card>
   );
 }
