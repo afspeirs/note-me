3e8a49578ae189c7e87e3c38ef8beb42d55450ae
Andy <andy@speirs.dev>
2023-08-08 18:28:08 +0100

    fix: add dependencies fix: use APP_VERSION env variable

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,11 @@ import { readFileSync } from 'fs';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-const appVersion = readFileSync('./.version', { encoding: 'utf8' }).replace('\n', '');
+const appVersion = readFileSync('./.version', { encoding: 'utf8' }).replace('\n', '') || 'N/A';
 
 // https://vitejs.dev/config/
 export default defineConfig({ // eslint-disable-line import/no-default-export
   define: {
-    'import.meta.env.PACKAGE_VERSION': JSON.stringify(process.env.npm_package_version),
     'import.meta.env.APP_VERSION': JSON.stringify(appVersion),
   },
   plugins: [
