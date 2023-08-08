6e36cca313db85ac3a5531555ed41b728ea34572
Andy Speirs <andy@speirs.dev>
2023-08-08 18:33:13 +0100

    feat: add semantic release

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
+import react from '@vitejs/plugin-react';
+import { readFileSync } from 'fs';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
+const appVersion = readFileSync('./.version', { encoding: 'utf8' }).replace('\n', '') || 'N/A';
 
 // https://vitejs.dev/config/
 export default defineConfig({ // eslint-disable-line import/no-default-export
   define: {
-    'import.meta.env.PACKAGE_VERSION': JSON.stringify(process.env.npm_package_version),
+    'import.meta.env.APP_VERSION': JSON.stringify(appVersion),
   },
   plugins: [
     react(),
