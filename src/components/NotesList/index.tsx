9d9b282f138eb36574517342e101a666e0f3aa2c
Andy <andy@speirs.dev>
2023-10-04 16:28:27 +0100

    feat: add virtualised support to the list of notes

--- src/components/NotesList/index.tsx
+++ src/components/NotesList/index.tsx
@@ -1,4 +1,6 @@
 import { useAtomValue } from 'jotai';
+import { useRef } from 'react';
+import { ViewportList } from 'react-viewport-list';
 import { useRxData } from 'rxdb-hooks';
 
 import type { NoteDocType } from '@/api/types';
@@ -7,6 +9,7 @@ import { notesSortAtom, notesSortOptions } from '@/context/notesSort';
 import { NotesListItem } from './NotesListItem';
 
 export function NotesList() {
+  const ref = useRef<HTMLUListElement | null>(null);
   const search = useAtomValue(notesSearchAtom);
   const sort = useAtomValue(notesSortAtom);
   const { result: notes, isFetching } = useRxData<NoteDocType>(
@@ -22,14 +25,23 @@ export function NotesList() {
   );
 
   return (
-    <ul role="list" className="flex flex-col p-2 gap-1 overflow-y-auto overflow-x-hidden h-full">
+    <ul
+      role="list"
+      className="flex flex-col h-full p-2 gap-1 overflow-y-auto overflow-x-hidden"
+      ref={ref}
+    >
       {isFetching && (
         <li className="block p-3">Loading...</li>
       )}
       {!isFetching && notes.length === 0 && (
         <li className="block p-3">No notes found</li>
       )}
-      {notes.map((note) => <NotesListItem key={note.id} note={note} />)}
+      <ViewportList
+        viewportRef={ref}
+        items={notes}
+      >
+        {(note) => <NotesListItem key={note.id} note={note} />}
+      </ViewportList>
     </ul>
   );
 }
