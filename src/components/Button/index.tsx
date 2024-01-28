a78df3357430ec2f1c9afd352465c18e50c94ba4
Andy <andy@speirs.dev>
2024-01-28 22:58:30 +0000

    fix: updated button to use ring-inset

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,7 +6,7 @@ import { classNames } from '@/utils/classNames';
 import type { ButtonProps } from './types';
 
 export const style = {
-  base: 'peer/button p-3 rounded-lg select-none disabled:opacity-40 disabled:pointer-events-none focus-visible',
+  base: 'peer/button p-3 rounded-lg select-none disabled:opacity-40 disabled:pointer-events-none focus-visible ring-inset',
   iconOnly: 'block',
   withText: 'flex items-center gap-4 min-w-0 w-full',
 } as const;
