406ce36c67bfd89ca5db386ca518eab03271f291
andy.speirs <andy.speirs@havas.com>
2024-03-08 12:21:46 +0000

    chore: import createRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { registerServiceWorker } from '@afspeirs/service-worker';
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
 import { App } from '@/App';
 import '@/index.css';
 import '@/webmanifest-apple';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
