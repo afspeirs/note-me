0ec8ece2f88cad8d8b052d9a60fd957caa1c8195
andy.speirs <andy.speirs@havas.com>
2024-03-08 12:10:37 +0000

    feat: use larger buttons on mobile

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,9 +6,9 @@ import { classNames } from '@/utils/classNames';
 import type { ButtonProps } from './types';
 
 export const style = {
-  base: 'peer/button rounded-lg select-none disabled:opacity-40 disabled:pointer-events-none focus-visible ring-inset',
-  iconOnly: 'block p-2',
-  withText: 'flex items-center gap-3 min-w-0 w-full px-3 py-2',
+  base: 'peer/button p-3 rounded-lg select-none disabled:opacity-40 disabled:pointer-events-none focus-visible ring-inset',
+  iconOnly: 'block sm:p-2',
+  withText: 'flex items-center gap-3 min-w-0 w-full sm:py-2',
 } as const;
 
 export const colours = {
