b048984dd7bb906f849ff8f15ac6925bac409e72
andy.speirs <andy.speirs@havas.com>
2024-03-10 20:42:12 +0000

    refactor: rename index.css to main.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from '@/App';
-import '@/index.css';
+import '@/main.css';
 import '@/webmanifest-apple';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
