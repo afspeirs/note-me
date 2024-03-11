99acf4d0be29711a449bce459def5edf9495b390
Andy <andy@speirs.dev>
2024-03-11 19:59:00 +0000

    feat: remove Notes ReSync from Settings

--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -7,7 +7,6 @@ import { ChangeTheme } from '@/components/ChangeTheme';
 import { CheckForUpdate } from '@/components/CheckForUpdate';
 import { NotesExport } from '@/components/NotesExport';
 import { NotesImport } from '@/components/NotesImport';
-import { NotesReSync } from '@/components/NotesReSync';
 import { Page } from '@/components/Page';
 import { Tooltip } from '@/components/Tooltip';
 
@@ -53,10 +52,6 @@ export function Settings() {
         <li>
           <NotesImport />
           <NotesExport />
-        </li>
-        <li aria-hidden="true"><hr className="mx-4 border-gray-300 dark:border-gray-400" /></li>
-        <li>
-          <NotesReSync />
         </li>
       </ul>
     </Page>
