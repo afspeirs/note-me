3e6e9d36a5ae343d07e9fef8c406fa0b018fe3be
andy.speirs <andy.speirs@havas.com>
2024-05-30 13:40:00 +0100

    feat: add blur to modal background

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -19,21 +19,21 @@ export function Modal({
       <Dialog as="div" className="relative z-50" onClose={onClose}>
         <Transition.Child
           as={Fragment}
-          enter="ease-out duration-300"
+          enter="ease-out duration-200"
           enterFrom="opacity-0"
           enterTo="opacity-100"
           leave="ease-in duration-200"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-primary/70 dark:bg-black/70" />
+          <div className="fixed inset-0 bg-primary/70 dark:bg-black/70 backdrop-blur-sm" />
         </Transition.Child>
 
         <div className={`fixed inset-0 mt-titlebar-area-height overflow-y-auto ${theme}`}>
           <div className="flex min-h-full items-center justify-center p-4 text-center">
             <Transition.Child
               as={Fragment}
-              enter="ease-out duration-300"
+              enter="ease-out duration-200"
               enterFrom="opacity-0 scale-95"
               enterTo="opacity-100 scale-100"
               leave="ease-in duration-200"
