13b3d9ba8813dc6dafd6175f4c88eb7f69aba408
Andy <andy@speirs.dev>
2025-01-19 21:26:44 +0000

    add session storage to sidebar context

--- src/lib/context/navigation.svelte.ts
+++ src/lib/context/navigation.svelte.ts
@@ -1,21 +1,25 @@
 export const mobileWidth = 1024;
 
 export const sidebarOpen = $state({
-  value: false,
+  value: JSON.parse(window.localStorage.getItem('note-me-sidebar-open') || 'false'),
   set(value: boolean) {
     this.value = value;
+    window.localStorage.setItem('note-me-sidebar-open', JSON.stringify(this.value));
   },
   toggle() {
     this.value = !this.value;
+    window.localStorage.setItem('note-me-sidebar-open', JSON.stringify(this.value));
   },
 });
 
 export const sidebarUseMobile = $state({
-  value: false,
+  value: JSON.parse(window.localStorage.getItem('note-me-sidebar-use-mobile') || 'false'),
   set(value: boolean) {
     this.value = value;
+    window.localStorage.setItem('note-me-sidebar-use-mobile', JSON.stringify(this.value));
   },
   toggle() {
     this.value = !this.value;
+    window.localStorage.setItem('note-me-sidebar-use-mobile', JSON.stringify(this.value));
   },
 });
