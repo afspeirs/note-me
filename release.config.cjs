6a994328232e8a3822ad8fc5508c6b577235a6a6
Andy <andy@speirs.dev>
2025-01-19 11:35:40 +0000

    add semantic release

---
+++ release.config.cjs
@@ -0,0 +1,33 @@
+function isDryRun() {
+  return process.argv.includes('--dry-run');
+}
+
+const configProduction = [
+  ['@semantic-release/npm', { npmPublish: false }],
+  [
+    '@semantic-release/git',
+    {
+      assets: ['.version', 'package.json'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    },
+  ],
+];
+
+module.exports = {
+  branches: [
+    'main',
+    { name: 'develop', prerelease: true },
+    { name: 'next', prerelease: true },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ...isDryRun() ? [] : configProduction,
+    [
+      '@semantic-release/exec',
+      {
+        verifyReleaseCmd: './scripts/release-update-version.sh ${nextRelease.version}',
+      },
+    ],
+  ],
+};
