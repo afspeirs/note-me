4c1e819ad998926023c91f67bc8504d672e89249
Andrew Speirs <andrew@speirs.dev>
2025-08-14 23:47:37 +0100

    feat: update to latest version of melt

--- src/lib/components/Toaster.svelte
+++ src/lib/components/Toaster.svelte
@@ -1,83 +1,66 @@
-<script lang="ts" context="module">
-  import type { CreateToasterProps } from '@melt-ui/svelte';
-
+<script lang="ts" module>
   type ToastData = {
     title: string;
     description?: string;
-    button?: {
+    action?: {
       title: string;
       onclick: () => void;
-    }
+    };
   };
-  type ToastAdd = ToastData & Omit<CreateToasterProps, 'hover'>;
 
-  const {
-    elements: { content, title, description, close },
-    helpers,
-    states: { toasts },
-    actions: { portal },
-  } = createToaster<ToastData>();
+  const toaster = new Toaster<ToastData>();
 
-  export const toast = {
-    add: ({ title, description, button, ...options }: ToastAdd) => helpers.addToast({
-      data: {
-        title,
-        description,
-        button,
-      },
-      ...options,
-    }),
-    remove: helpers.removeToast,
-    update: helpers.updateToast,
-  };
+  export const addToast = toaster.addToast;
 </script>
 
 <script lang="ts">
-  import { createToaster, melt } from '@melt-ui/svelte';
+  import { Toaster } from 'melt/builders';
   import { XIcon } from 'lucide-svelte';
   import { flip } from 'svelte/animate';
   import { fly } from 'svelte/transition';
+
+  // Need to used $derived to ensure reactivity between the Toaster and the module
+  const toasts = $derived(toaster.toasts);
 </script>
 
-<div class="fixed right-0 bottom-0 flex flex-col items-end gap-2 m-4 z-50 select-none" use:portal>
-  {#each $toasts as { id, data } (id)}
-    <div
-      animate:flip={{ duration: 500 }}
-      in:fly={{ duration: 150, x: '100%' }}
-      out:fly={{ duration: 150, x: '100%' }}
-      class="rounded-lg bg-dark dark:bg-white text-white dark:text-dark shadow-md"
-      use:melt={$content(id)}
-    >
-      <div class="relative min-w-36 max-w-[calc(100vw-2rem)] p-2">
-        <div class="flex gap-2 font-semibold">
-          <h3
-            class="flex items-center gap-2 px-2"
-            use:melt={$title(id)}
-          >
-            {data.title}
-          </h3>
-          {#if data.button}
+<div {...toaster.root}>
+  <div class="fixed right-0 bottom-0 flex flex-col items-end gap-2 m-4 z-50 select-none">
+    {#each toasts as toast (toast.id)}
+      <div
+        animate:flip={{ duration: 500 }}
+        in:fly={{ duration: 150, x: '100%' }}
+        out:fly={{ duration: 150, x: '100%' }}
+        class="rounded-lg bg-dark dark:bg-white text-white dark:text-dark shadow-md"
+        {...toast.content}
+      >
+        <div class="relative min-w-36 max-w-[calc(100vw-2rem)] p-2">
+          <div class="flex gap-2 font-semibold">
+            <h3 class="flex items-center gap-2 px-2" {...toast.title}>
+              {toast.data.title}
+            </h3>
+            {#if toast.data.action}
+              <button
+                class="p-2 rounded-md text-dark dark:text-white bg-white dark:bg-dark hover:bg-white/80 dark:hover:bg-dark/80 cursor-pointer"
+                onclick={toast.data.action.onclick}
+              >
+                <span>{toast.data.action.title}</span>
+              </button>
+            {/if}
             <button
-              class="p-2 rounded-md text-dark dark:text-white bg-white dark:bg-dark hover:bg-white/80 dark:hover:bg-dark/80"
-              onclick={data.button.onclick}
+              class="p-2 rounded-md text-white dark:text-dark hover:bg-white/10 dark:hover:bg-black/10 cursor-pointer"
+              {...toast.close}
             >
-              <span>{data.button.title}</span>
+              <XIcon class="size-6" aria-hidden="true" />
+              <span class="sr-only">Close notification</span>
             </button>
-          {/if}
-          <button
-            class="p-2 rounded-md text-white dark:text-dark hover:bg-white/10 dark:hover:bg-black/10"
-            use:melt={$close(id)}
-          >
-            <XIcon class="size-6" aria-hidden="true" />
-            <span class="sr-only">Close notification</span>
-          </button>
-        </div>
-        {#if data.description}
-          <div use:melt={$description(id)}>
-            {data.description}
           </div>
-        {/if}
+          {#if toast.data.description}
+            <div {...toast.description}>
+              {toast.data.description}
+            </div>
+          {/if}
+        </div>
       </div>
-    </div>
-  {/each}
+    {/each}
+  </div>
 </div>
--- src/lib/utils/copyToClipboard.ts
+++ src/lib/utils/copyToClipboard.ts
@@ -1,17 +1,21 @@
-import { toast } from '$lib/components/Toaster.svelte';
+import { addToast } from '$lib/components/Toaster.svelte';
 
 export function copyToClipBoard(text: string) {
   navigator.clipboard
     .writeText(text)
     .then(() => {
-      toast.add({
-        title: `"${text}" was copied to your clipboard`,
+      addToast({
+        data: {
+          title: `"${text}" was copied to your clipboard`,
+        },
       });
       console.log(`"${text}" was copied to your clipboard`); // eslint-disable-line no-console
     })
     .catch((error) => {
-      toast.add({
-        title: `An error occurred: "${error}"`,
+      addToast({
+        data: {
+          title: `An error occurred: "${error}"`,
+        },
       });
       console.error(error); // eslint-disable-line no-console
     });
