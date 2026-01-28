d2dbb5f8c9c96587b025d0869a02ed3b0f5a3730
Andrew Speirs <andrew@speirs.dev>
2026-01-28 18:41:02 +0000

    feat: remove support for apple icons

--- src/lib/utils/webmanifest.ts
+++ src/lib/utils/webmanifest.ts
@@ -1,7 +1,13 @@
-export function getWebmanifest(manifest: string, apple = false) {
+const branchNames = [
+  'local',
+  'develop',
+  'next',
+];
+
+export function getWebmanifest(manifest: string) {
   const webmanifest = JSON.parse(manifest);
   const branchName = process?.env?.HEAD || 'local';
-  const imageBranch = ['local', 'develop', 'next'].includes(branchName) ? `_${branchName}` : '';
+  const imageBranch = branchNames.includes(branchName) ? `_${branchName}` : '';
 
   if (branchName && branchName !== 'main') {
     webmanifest.name += ` (${branchName})`;
@@ -9,16 +15,6 @@ export function getWebmanifest(manifest: string, apple = false) {
 
   if (imageBranch !== '') {
     webmanifest.icons[0].src = `/icon-maskable-512x512${imageBranch}.png`;
-  }
-
-  if (apple) {
-    webmanifest.icons = [
-      {
-        src: `/icon-apple-512x512${imageBranch}.png`,
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ];
   }
 
   return JSON.stringify(webmanifest, null, 2);
--- src/routes/site-apple.webmanifest/+server.ts
+++
@@ -1,9 +0,0 @@
-import { getWebmanifest } from '$lib/utils/webmanifest';
-import manifestRaw from '../site.webmanifest/site.webmanifest?raw';
-
-export function GET() {
-  const newManifest = getWebmanifest(manifestRaw, true);
-  return new Response(newManifest);
-}
-
-export const prerender = true;
